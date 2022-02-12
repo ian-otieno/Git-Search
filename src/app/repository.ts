bef83b60de97cc57f816fb3a1ed0a560371659f9
ian_otieno <ian.otieno@student.moringaschol.com>
2022-02-12 13:30:36 +0300

    add constructors to class user and repository

--- src/app/repository.ts
+++ src/app/repository.ts
@@ -1,2 +1,10 @@
 export class Repository {
+    constructor(
+        public html_url:string,
+        public name:string,
+        public description:string,
+        public updated_at: Date,
+        public clone_url:string,
+        public language:string,
+    ){}  
 }
--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,11 @@
 export class User {
+    constructor(
+        public name:string,
+        public avatar_url:string,
+        public login: string,
+        public bio:string,
+        public company:string,
+        public public_repos:number,
+        public created_at:Date
+    ){ }   
 }
