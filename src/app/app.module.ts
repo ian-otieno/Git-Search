31d3a277a948ba0dd03d8b91710daa1ea21a20b4
ian_otieno <ian.otieno@student.moringaschol.com>
2022-02-12 13:14:46 +0300

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyProfileComponent } from './my-profile/my-profile.component';
+import { MyRepositoriesComponent } from './my-repositories/my-repositories.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchFormComponent } from './search-form/search-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyProfileComponent,
+    MyRepositoriesComponent,
+    NavbarComponent,
+    SearchFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/my-repositories/my-repositories.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-repositories',
+  templateUrl: './my-repositories.component.html',
+  styleUrls: ['./my-repositories.component.css']
+})
+export class MyRepositoriesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
