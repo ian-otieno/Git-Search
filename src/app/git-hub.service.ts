ac76d4bb1809fb70405cee7595905a26fbfd110b
ian_otieno <ian.otieno@student.moringaschol.com>
2022-02-12 15:21:43 +0300

    add constructor to git-hub service

--- src/app/git-hub.service.ts
+++ src/app/git-hub.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class GitHubService {
 
-  constructor() { }
+ private username:string
+  private myRepos: any
+
+
+  constructor(private http:HttpClient) { 
+    this.username="ian-otieno"
+    this.myRepos="repos"
+  }
+  getProfileData(){
+    return this.http.get("https://api.github.com/users/ian-otieno")
+  };
+  getMyRepos(){
+    return this.http.get("https://api.github.com/users/ian-otieno/repos")
+  }
+
 }
