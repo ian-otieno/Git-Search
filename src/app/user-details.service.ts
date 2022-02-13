2eb0eeed6fa97eff125d9bd8d447062988b1a79f
ian_otieno <ian.otieno@student.moringaschol.com>
2022-02-13 08:03:46 +0300

    add get user details to user-details service

--- src/app/user-details.service.ts
+++ src/app/user-details.service.ts
@@ -16,7 +16,17 @@ export class UserDetailsService {
     this.user= new User("", "", "", "", "", 0, new Date())
     this.repository= new Repos("","", "", new Date(), "", "")
    }
-  
+   getUserDetails(username:string){
+    interface ApiResponse{
+      name:string,
+      avatar_url:string,
+      login: string,
+      bio:string,
+      company:string,
+      public_repos:number,
+      created_at:Date
+    }
+    
 
 
 }
