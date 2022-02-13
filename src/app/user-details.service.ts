1a16174e0f4ff26e31f3f183282c4f307a0c9480
ian_otieno <ian.otieno@student.moringaschol.com>
2022-02-13 08:05:02 +0300

    add parameters to user details service

--- src/app/user-details.service.ts
+++ src/app/user-details.service.ts
@@ -26,7 +26,32 @@ export class UserDetailsService {
       public_repos:number,
       created_at:Date
     }
-    
+    let promise =new Promise<void>((resolve,reject)=>{
+      this.http.get<ApiResponse>("https://api.github.com/users/" +username).toPromise().then(response=>{
+        this.user.name=response.name
+        this.user.avatar_url=response.avatar_url
+        this.user.login=response.login
+        this.user.bio=response.bio
+        this.user.company=response.company
+        this.user.public_repos=response.public_repos
+        this.user.created_at=response.created_at
+
+        resolve()
+      },error=>{
+        reject(error)
+      })
+      this.http.get<any>("https://api.github.com/users/" +username+"/repos").toPromise().then(response=>{
+          for(let j=0; j<response.length; j++){
+            this.newUserData=new Repos(response[j].html_url, response[j].name, response[j].description, response[j].updated_at, response[j].clone_url, response[j].language);
+            this.repoData.push(this.newUserData)
+          }
+          resolve()
+      },error=>{
+        reject(error)
+      })
+    })
+    return promise
+  }   
 
 
 }
